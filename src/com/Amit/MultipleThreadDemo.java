e0145f5e0d7bc173b2655c59628e254820cfb677
Amit <amit.j.joglekar@gmail.com>
2013-11-29 23:39:07 +0530

    hello world

--- src/com/Amit/MultipleThreadDemo.java
+++ src/com/Amit/MultipleThreadDemo.java
@@ -1,11 +1,9 @@
 package com.Amit;
 
-/**
- * Created by Amit on 24/11/13.
- */
 public class MultipleThreadDemo {
     public static void main(String[] args) {
         Test t = new Test();
+        t.wait(0x64);
         Thread t1 = new Thread(t);
         Thread t2 = new Thread(t);
         Thread t3 = new Thread(t);
